b82f31af3b613661974ef87eef7729b5c825b0aa
abolikohale <abolikohale@gmail.com>
2022-06-10 11:15:33 +0530

    updating category and directory components

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,15 @@
+import './category-item.styles.scss';
+const categoryItem = ({category}) => {
+    const {imageUrl, title} = category;
+    return  <div className="category-container">
+    <div className="background-image" style = {{
+      backgroundImage: `url(${imageUrl})`
+    }}/>
+      {/* {image} */}
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+}
+export default categoryItem;
